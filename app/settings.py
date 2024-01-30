02296f4fb93b9be7c3376e88ee5a2fe40585c720
Oleksandr Nychyk <nychyk.oleksandr@lll.kpi.ua>
2024-01-30 22:58:44 +0200

    test commit

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,32 @@
 import os 
 
-class Settings:
+# class Settings:
     
-    TOKEN: str = os.getenv("TOKEN")
-    CHANNEL_URL: str = os.getenv("CHANNEL_URL")
-    CHAT_ID: int = int(os.getenv("CHAT_ID"))
+#     TOKEN: str = os.getenv("TOKEN")
+#     CHANNEL_URL: str = os.getenv("CHANNEL_URL")
+#     CHAT_ID: int = int(os.getenv("CHAT_ID"))
+#     ADMIN_ID: str = os.getenv("ADMIN_ID")
+
+from pydantic import AnyUrl, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DEVELOPMENT: bool = True
+    DEBUG: bool = True
+    TOKEN: SecretStr
+    CHAT_ID: int
+    TELEGRAM_SECRET: SecretStr
+    BASE_URL: AnyUrl = AnyUrl("http://localhost:8000")
+    WEBHOOK_PATH: str
     ADMIN_ID: str = os.getenv("ADMIN_ID")
+    @property
+    def WEBHOOK_URL(self) -> str:
+        return f"{self.BASE_URL}{self.WEBHOOK_PATH}"
+
+    model_config = SettingsConfigDict(
+        env_file=('.env', 'stack.env'), env_file_encoding='utf-8', extra='ignore')
+
+
+
+settings = Settings()  # type: ignore[call-arg]
