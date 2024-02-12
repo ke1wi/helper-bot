9757845bb8400488d4d8388cb17a253531fccfdb
Oleksandr Nychyk <nychyk.oleksandr@lll.kpi.ua>
2024-02-12 21:04:49 +0200

    setting refact

--- app/settings.py
+++ app/settings.py
@@ -15,11 +15,16 @@ class Settings(BaseSettings):
     BASE_URL: AnyUrl = AnyUrl("http://localhost:8000")
     WEBHOOK_PATH: str
 
+    HOST: SecretStr
+    PORT: int
+    PASSWORD: SecretStr
+
     @property
     def WEBHOOK_URL(self) -> str:
         return f"{self.BASE_URL}{self.WEBHOOK_PATH}"
 
-    model_config = SettingsConfigDict(env_file=('.env', 'stack.env'), env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(
+        env_file=('.env', 'stack.env'), env_file_encoding='utf-8', extra='ignore')
 
 
 settings = Settings()
